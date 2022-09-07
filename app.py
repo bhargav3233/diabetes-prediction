608b69743b349b999b278d40e802457c9d0535c5
bhargav3233 <109935418+bhargav3233@users.noreply.github.com>
2022-09-07 13:13:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ import pickle
 import streamlit as st
 
 # loading the saved model
-loaded_model = pickle.load(open('D:/MLProjects/Diabetes/diabetes.sav', 'rb'))
+loaded_model = pickle.load(open('C:/diabetes/diabetes.sav', 'rb'))
+
+
+
+# creating a function for Prediction
 
 def diabetes_prediction(input_data):
     
@@ -23,35 +27,41 @@ def diabetes_prediction(input_data):
       return 'The person is diabetic'
   
     
+  
 def main():
-      
-      
-      # giving a title
-      st.title('Diabetes Prediction Web App')
-      
-      
-      # getting the input data from the user
-      
-      
-      Pregnancies = st.text_input('Number of Pregnancies')
-      Glucose = st.text_input('Glucose Level')
-      BloodPressure = st.text_input('Blood Pressure value')
-      SkinThickness = st.text_input('Skin Thickness value')
-      Insulin = st.text_input('Insulin Level')
-      BMI = st.text_input('BMI value')
-      DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
-      Age = st.text_input('Age of the Person')
-      
-      
-      # code for Prediction
-      diagnosis = ''
-      
-      # creating a button for Prediction
-      
-      if st.button('Diabetes Test Result'):
-          diagnosis = diabetes_prediction([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
-         
-      st.success(diagnosis)
+    
+    
+    # giving a title
+    st.title('Diabetes Prediction Web App')
+    
+    
+    # getting the input data from the user
+    
+    
+    Pregnancies = st.text_input('Number of Pregnancies')
+    Glucose = st.text_input('Glucose Level')
+    BloodPressure = st.text_input('Blood Pressure value')
+    SkinThickness = st.text_input('Skin Thickness value')
+    Insulin = st.text_input('Insulin Level')
+    BMI = st.text_input('BMI value')
+    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+    Age = st.text_input('Age of the Person')
+    
+    
+    # code for Prediction
+    diagnosis = ''
+    
+    # creating a button for Prediction
+    
+    if st.button('Diabetes Test Result'):
+        diagnosis = diabetes_prediction([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
+        
+        
+    st.success(diagnosis)
+    
+    
+    
+    
     
 if __name__ == '__main__':
     main()
